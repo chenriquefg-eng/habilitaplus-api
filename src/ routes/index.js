32a63c08151cceb5d2aa01a77afe6f8a9cd7b285
chenriquefg-eng <chenriquefg@gmail.com>
2025-12-12 17:56:49 -0300

    Update index.js

--- src/ routes/index.js
+++ src/ routes/index.js
@@ -3,12 +3,10 @@ import instrutoresRoutes from './instrutores.routes.js';
 
 const router = Router();
 
-// rota de teste
 router.get('/health', (req, res) => {
   res.json({ status: 'ok', app: 'HabilitaPlus API' });
 });
 
-// rotas do sistema
 router.use('/instrutores', instrutoresRoutes);
 
 export default router;
