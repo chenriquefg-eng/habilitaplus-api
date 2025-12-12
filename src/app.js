10889e7d810d55f961b1a167538d6b85d1f26b89
chenriquefg-eng <chenriquefg@gmail.com>
2025-12-12 18:46:05 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
-import fs from 'fs';
-
-console.log(
-  fs.existsSync(new URL('./routes/index.js', import.meta.url))
-);
-
 import express from 'express';
 import cors from 'cors';
+
 import routes from './routes/index.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+
+// rotas
+app.use('/', routes);
 
 export default app;
