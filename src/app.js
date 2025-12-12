4e9f1094452fa0d47bc42ee08f0afa22a8955786
chenriquefg-eng <chenriquefg@gmail.com>
2025-12-12 18:38:44 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+import fs from 'fs';
+
+console.log(
+  fs.existsSync(new URL('./routes/index.js', import.meta.url))
+);
+
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/index.js';
