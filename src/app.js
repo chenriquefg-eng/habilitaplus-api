acf176371e3a424bac1659b9d20b4fb92d2b0bbf
chenriquefg-eng <chenriquefg@gmail.com>
2025-12-13 10:49:28 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes/index.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(routes);
-app.use('/', routes);
+// rota de teste direta (sem router)
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+// rota direta para testar
+app.get('/instrutores', (req, res) => {
+  res.json({ message: 'Lista de instrutores' });
+});
 
 export default app;
