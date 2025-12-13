ee3b50e0f0dacd2fc56b25a02edf6d5fca81e9bf
chenriquefg-eng <chenriquefg@gmail.com>
2025-12-12 21:19:19 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-// rotas
 app.use('/', routes);
 
 export default app;
