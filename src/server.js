f1652bd71b39551634af1433b8de6220a20a279f
chenriquefg-eng <chenriquefg@gmail.com>
2025-12-12 23:17:33 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import app from './app.js';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 API rodando na porta ${PORT}`);
+  console.log(`API rodando na porta ${PORT}`);
 });
