a713930e25b7b80665131f588bec03ced04cfb73
chenriquefg-eng <chenriquefg@gmail.com>
2025-12-12 21:26:57 -0300

    Update instrutores.routes.js

--- src/ routes/instrutores.routes.js
+++ src/ routes/instrutores.routes.js
@@ -3,10 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.json({
-    message: 'Lista de instrutores',
-    data: []
-  });
+  res.json({ message: 'Lista de instrutores' });
 });
 
 export default router;
