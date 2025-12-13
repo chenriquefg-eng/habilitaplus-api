b542c3b50585d94b9d82718942251a933ffa4d86
chenriquefg-eng <chenriquefg@gmail.com>
2025-12-12 23:24:20 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import app from './app.js';
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`API rodando na porta ${PORT}`);
+  console.log(`🚀 API rodando na porta ${PORT}`);
 });
