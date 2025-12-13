cc51bd0f1ceb404786b5080e01e5a36833224607
chenriquefg-eng <chenriquefg@gmail.com>
2025-12-12 23:04:44 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes/index.js';
 
 const app = express();
 
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok', app: 'HabilitaPlus API' });
-});
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
 
 export default app;
