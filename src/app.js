76c65d2bcfb850d7f9413225c57e2d353e29b8b0
chenriquefg-eng <chenriquefg@gmail.com>
2026-04-24 11:01:11 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('🚀 API HabilitaPlus rodando');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok', app: 'HabilitaPlus API' });
+});
+
 app.use('/', routes);
 
 export default app;
