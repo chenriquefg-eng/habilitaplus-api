16f0e1b17ecfc5c42a4c7f25ad110715c35ff694
chenriquefg-eng <chenriquefg@gmail.com>
2026-04-24 19:02:35 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import pool from './db/pool.js';
 import express from 'express';
 import cors from 'cors';
+import pool from './db/pool.js';
 
 const app = express();
 
@@ -14,42 +14,14 @@ app.get('/', (req, res) => {
 app.get('/health', (req, res) => {
   res.json({ status: 'ok', app: 'HabilitaPlus API' });
 });
-app.put('/aulas/:id/aceitar', (req, res) => {
-  const id = parseInt(req.params.id);
 
-  const aula = aulas.find(a => a.id === id);
-
-  if (!aula) {
-    return res.status(404).json({
-      status: 'erro',
-      mensagem: 'Aula não encontrada'
-    });
-  }
-
-  aula.status = 'aceita';
-  const valorInstrutor = aula.valor * 0.7;
-const valorApp = aula.valor * 0.3;
-
-aula.pagamento = {
-  total: aula.valor,
-  instrutor: valorInstrutor,
-  app: valorApp
-};
-  aula.instrutor = req.body.instrutor || 'Não informado';
-
-  res.json({
-    status: 'ok',
-    mensagem: 'Aula aceita com sucesso',
-    aula
-  });
-});
-export default app;
 app.get('/alunos', (req, res) => {
   res.json({
     status: 'ok',
     mensagem: 'Rota de alunos ativa. Use POST para cadastrar.'
   });
 });
+
 app.post('/alunos', (req, res) => {
   const aluno = req.body;
 
@@ -59,9 +31,7 @@ app.post('/alunos', (req, res) => {
     aluno
   });
 });
-let aulas = [];
 
-import pool from './db/pool.js';
 app.get('/aulas', async (req, res) => {
   try {
     const result = await pool.query(`
@@ -76,7 +46,6 @@ app.get('/aulas', async (req, res) => {
       total: result.rows.length,
       aulas: result.rows
     });
-
   } catch (error) {
     res.status(500).json({
       status: 'erro',
@@ -84,22 +53,5 @@ app.get('/aulas', async (req, res) => {
     });
   }
 });
-app.put('/aulas/:id/concluir', (req, res) => {
-  const id = parseInt(req.params.id);
-  const aula = aulas.find(a => a.id === id);
 
-  if (!aula) {
-    return res.status(404).json({
-      status: 'erro',
-      mensagem: 'Aula não encontrada'
-    });
-  }
-
-  aula.status = 'concluida';
-
-  res.json({
-    status: 'ok',
-    mensagem: 'Aula concluída',
-    aula
-  });
-});
+export default app;
